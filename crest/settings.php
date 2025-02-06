7aa9e6e20ec13bbabb02680205f8856d03d9c28c
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-06 10:22:10 +0530

    Initial commit

---
+++ crest/settings.php
@@ -0,0 +1,5 @@
+<?php
+define('C_REST_WEB_HOOK_URL', 'https://gicrm.ae/rest/1945/7mnw3te56u363prw/');
+define('C_REST_CLIENT_ID', 'local.67a43f766e9658.47576452');
+define('C_REST_CLIENT_SECRET', 'r63lH76D0097uhT5w7MaAf4w9qohd2ZE216887uylFUrEHno4m');
+define('C_REST_BLOCK_LOG', true);
